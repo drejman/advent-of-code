e4587bc3d4163aa85429f304b34501085ab93175
Dominik Rejman <dominik.rejman@nokia.com>
2023-12-02 00:06:41 +0100

    Reworked solution to day 1

--- solutions/2023/day_01/solution.py
+++ solutions/2023/day_01/solution.py
@@ -5,8 +5,8 @@
 #  has been amended by a very young Elf who was apparently just excited to show off her art skills.
 #  Consequently, the Elves are having trouble reading the values on the document.
 
-# The newly-improved calibration document consists of lines of text; 
-# each line originally contained a specific calibration value that the Elves now need to recover. 
+# The newly-improved calibration document consists of lines of text;
+# each line originally contained a specific calibration value that the Elves now need to recover.
 # On each line, the calibration value can be found by combining the first digit and the last digit (in that order)
 #  to form a single two-digit number.
 
@@ -21,7 +21,6 @@
 
 # Consider your entire calibration document. What is the sum of all of the calibration values?
 
-
 from ...base import StrSplitSolution, answer
 
 
@@ -29,75 +28,61 @@ class Solution(StrSplitSolution):
     _year = 2023
     _day = 1
 
-    numbers = {'one': "1",
-               'two': "2",
-               'three': "3",
-               'four': "4",
-               'five': "5",
-               'six': "6",
-               'seven': "7",
-               'eight': "8",
-               'nine': "9",
-               }
+    words = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    numbers = {
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5": "5",
+        "6": "6",
+        "7": "7",
+        "8": "8",
+        "9": "9",
+    }
+    window = max([len(key) for key in words.keys()])
 
     @answer(54697)
     def part_1(self) -> int:
-        numbers = []
-        for line in self.input:
-            self.debug(f"word: {line}")
-            try:
-                digits = self._find_all_digits(word=line)
-            except ValueError:
-                continue
-            first_digit = digits[min(digits.keys())]
-            last_digit = digits[max(digits.keys())]
-            numbers.append(int(first_digit+last_digit))
-        return sum(numbers)
-
-
-    @staticmethod
-    def _find_first_digit(word: str) -> str:
-        for char in word:
-            if char.isdigit():
-                return char
-        raise ValueError(f"No digits found in {word}")
-    
-    @staticmethod
-    def _find_last_digit(word: str) -> str:
-        for char in word[::-1]:
-            if char.isdigit():
-                return char
-        raise ValueError(f"No digits found in {word}")
-    
-    @classmethod
-    def _find_all_digits(cls, word: str, words_also: bool = False) -> dict[int, str]:
-        digits = {}
-        for ix, char in enumerate(word):
-            if char.isdigit():
-                digits[ix] = char
-            if words_also:
-                for number, value in cls.numbers.items():
-                    if (index := word.find(number, ix, ix+5)) != -1:
-                        digits[index] = value
-        if not digits:
-            raise ValueError(f"No digits found in {word}")
-        return digits
-
+        return self.solve_part(words_also=False)
 
     @answer(54885)
     def part_2(self) -> int:
-        numbers = []
+        return self.solve_part(words_also=True)
+
+    def solve_part(self, words_also: bool):
+        result = 0
         for line in self.input:
             self.debug(f"word: {line}")
             try:
-                digits = self._find_all_digits(word=line, words_also=True)
+                digits = self._find_all_digits(word=line, words_also=words_also)
             except ValueError:
                 continue
             first_digit = digits[min(digits.keys())]
             last_digit = digits[max(digits.keys())]
-            numbers.append(int(first_digit+last_digit))
-        return sum(numbers)
+            result += int(first_digit + last_digit)
+        return result
 
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+    @classmethod
+    def _find_all_digits(cls, word: str, words_also: bool = False) -> dict[int, str]:
+        digits = {}
+        numbers = cls.numbers.copy()
+        if words_also:
+            numbers.update(cls.words)
+        for ix, _ in enumerate(word):
+            for number, value in numbers.items():
+                if (index := word.find(number, ix, ix + cls.window)) != -1:
+                    digits[index] = value
+        if not digits:
+            raise ValueError(f"No digits found in {word}")
+        return digits
