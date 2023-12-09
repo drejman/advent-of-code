32387aabb40263340ede81828b4d22b5960de7d0
Dominik Rejman <dominik.rejman@nokia.com>
2023-12-10 00:01:42 +0100

    Day 7 refactor

--- solutions/2023/day_07/solution.py
+++ solutions/2023/day_07/solution.py
@@ -3,32 +3,32 @@
 # puzzle prompt: https://adventofcode.com/2023/day/7
 from collections import Counter
 from string import digits
+from typing import Callable
 
-from ...base import StrSplitSolution, answer
+from ...base import StrSplitSolution, answer, timeit
 
 
 class Solution(StrSplitSolution):
     _year = 2023
     _day = 7
 
+    @timeit
     @answer(250370104)
     def part_1(self) -> int:
         self._parse_input()
         self.game.sort(key=lambda x: self._sort_hand_part1(x[0]))
-        self.debug(self.game)
         return self._get_result()
 
+    @timeit
     @answer(251735672)
     def part_2(self) -> int:
         self._parse_input()
         self.game.sort(key=lambda x: self._sort_hand_part2(x[0]))
-        self.debug(self.game)
         return self._get_result()
 
     def _get_result(self):
         result = 0
         for ix, (hand, bid) in enumerate(self.game):
-            self.debug(f"{ix+1} {hand} {bid}")
             result += (ix + 1) * bid
         return result
 
@@ -38,35 +38,40 @@ class Solution(StrSplitSolution):
             hand, bid = line.split()
             self.game.append((hand, int(bid)))
 
-    @staticmethod
-    def _sort_hand_part1(hand: str) -> int:
+    @classmethod
+    def _sort_hand_part1(cls, hand: str) -> int:
         cards = digits + "TJQKA"
-        counter = Counter(hand)
-        counts = list(sorted(counter.values(), reverse=True))
-        counts.append(0)  # hack to get rid of IndexErrors
-        rank = counts[0] * 10 + counts[1]
+        func = cls._sort_template_method(cards_order=cards)
+        return func(hand)
 
-        score = 0
-        levels = len(hand)
-        for ix, card in enumerate(hand):
-            score += cards.index(card) * (100 ** (levels - ix))
-        score += rank * (100 ** (levels + 1))
-        return score
+    @classmethod
+    def _sort_hand_part2(cls, hand: str) -> int:
+        cards = "J" + digits + "TQKA"
+        func = cls._sort_template_method(cards_order=cards, jokers="J")
+        return func(hand)
 
     @staticmethod
-    def _sort_hand_part2(hand: str) -> int:
-        cards = "J" + digits + "TQKA"
-        score = 0
-        levels = len(hand)
-        for ix, card in enumerate(hand):
-            score += cards.index(card) * (100 ** (levels - ix))
-
-        counter = Counter((card for card in hand if card != "J"))
-        counts = list(sorted(counter.values(), reverse=True))
-        counts.extend([0, 0])  # hack to get rid of IndexErrors
-        for _ in (card for card in hand if card == "J"):
-            if counts[0] < 5:
+    def _sort_template_method(
+        cards_order: str, jokers: str = ""
+    ) -> Callable[[str], int]:
+        def _sort(hand: str) -> int:
+            # start with finding type (four-of-kind etc)
+            # based on two cards (because of full house) with highest count
+            counter = Counter((card for card in hand if card not in jokers))
+            counts = list(sorted(counter.values(), reverse=True))
+            counts.extend([0] * (len(jokers) + 1))  # hack to get rid of IndexErrors
+            # jokers are treated as cards of highest count (always most beneficial)
+            for _ in (card for card in hand if card in jokers):
                 counts[0] += 1
-        rank = counts[0] * 10 + counts[1]
-        score += rank * (100 ** (levels + 1))
-        return score
+            # type of hand has highest impact so is multiplied by highiest factor
+            type_score = counts[0] * 10 + counts[1]
+            levels = len(hand)
+            score = type_score * (100 ** (levels + 1))
+
+            # last part of score is highest card
+            # starting from the left (highest factor) to the right (lowest factor)
+            for ix, card in enumerate(hand):
+                score += cards_order.index(card) * (100 ** (levels - ix))
+            return score
+
+        return _sort
