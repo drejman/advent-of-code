aa67f2b1acdb4ac2aaa3f9c6efbfb3ef77074eaa
Dominik Rejman <dominik.rejman@nokia.com>
2023-12-13 10:55:38 +0100

    Reformat

--- solutions/2023/day_10/solution.py
+++ solutions/2023/day_10/solution.py
@@ -27,14 +27,14 @@ class Solution(StrSplitSolution):
             "L": chr(int("0x2514", 0)),
             "F": chr(int("0x250C", 0)),
             "J": chr(int("0x2518", 0)),
-            }
-        self.char_mapping["S"]=self.char_mapping["7"]
+        }
+        self.char_mapping["S"] = self.char_mapping["7"]
         input_ = ""
         count = 0
         for h, line in enumerate(self.input):
             in_ = False
             for w, _ in enumerate(line):
-                if self.graph.get((w,h)):
+                if self.graph.get((w, h)):
                     if self.input[h][w] in "|7FS":
                         in_ = not in_
                     input_ += self.char_mapping[self.input[h][w]]
@@ -47,7 +47,6 @@ class Solution(StrSplitSolution):
             input_ += "\n"
         self.debug(input_)
         return count
-
 
     def _initialize_graph(self):
         self.height = len(self.input)
@@ -73,7 +72,7 @@ class Solution(StrSplitSolution):
         for initial_shift_w, initial_shift_h in neighborhood:
             w, h = coordinates
             shift_w, shift_h = initial_shift_w, initial_shift_h  # 0, 1
-            char = self.input[h+shift_h][w+shift_w]  # | [3][0]
+            char = self.input[h + shift_h][w + shift_w]  # | [3][0]
             self.debug(f"{char} [{h+shift_h}] [{w+shift_w}]")
             counter = 0
             while char != "S":
@@ -82,7 +81,9 @@ class Solution(StrSplitSolution):
                         break
                     case ["|", 0, -1 | 1]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))  # [0,2] = [(0,3), ]
+                        self.graph[(w, h)].append(
+                            (w + shift_w, h + shift_h)
+                        )  # [0,2] = [(0,3), ]
 
                         # new coordinates
                         w += shift_w  # 0
@@ -94,122 +95,130 @@ class Solution(StrSplitSolution):
                         shift_w = shift_w  # 0
                     case ["-", -1 | 1, 0]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))  # [1,3] = [(2,3), ]
+                        self.graph[(w, h)].append(
+                            (w + shift_w, h + shift_h)
+                        )  # [1,3] = [(2,3), ]
 
                         # new coordinates
                         w += shift_w  # 2
                         h += shift_h  # 3
                         self.graph[(w, h)] = []  # [2,3] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
                         shift_h = shift_h  # 0
                         shift_w = shift_w  # 1
                     case ["L", -1, 0]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))
+                        self.graph[(w, h)].append((w + shift_w, h + shift_h))
 
                         # new coordinates
                         w += shift_w
                         h += shift_h
                         self.graph[(w, h)] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
                         shift_h = -1
                         shift_w = 0
                     case ["L", 0, 1]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h)) # [0,3] = [(0,4), ]
+                        self.graph[(w, h)].append(
+                            (w + shift_w, h + shift_h)
+                        )  # [0,3] = [(0,4), ]
 
                         # new coordinates
                         w += shift_w  # 0
                         h += shift_h  # 4
                         self.graph[(w, h)] = []  # [0,4] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
-                        shift_h = 0 
+                        shift_h = 0
                         shift_w = 1
                     case ["J", 1, 0]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h)) # [0,4] = [(1,4), ]
+                        self.graph[(w, h)].append(
+                            (w + shift_w, h + shift_h)
+                        )  # [0,4] = [(1,4), ]
 
                         # new coordinates
                         w += shift_w  # 1
                         h += shift_h  # 4
                         self.graph[(w, h)] = []  # [1,4] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
-                        shift_h = -1 
+                        shift_h = -1
                         shift_w = 0
                     case ["J", 0, 1]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))
+                        self.graph[(w, h)].append((w + shift_w, h + shift_h))
 
                         # new coordinates
                         w += shift_w
                         h += shift_h
                         self.graph[(w, h)] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
-                        shift_h = 0 
+                        shift_h = 0
                         shift_w = -1
                     case ["F", 0, -1]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))
+                        self.graph[(w, h)].append((w + shift_w, h + shift_h))
 
                         # new coordinates
                         w += shift_w
                         h += shift_h
                         self.graph[(w, h)] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
                         shift_h = 0
                         shift_w = 1
                     case ["F", -1, 0]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))
+                        self.graph[(w, h)].append((w + shift_w, h + shift_h))
 
                         # new coordinates
                         w += shift_w
                         h += shift_h
                         self.graph[(w, h)] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
                         shift_h = 1
                         shift_w = 0
                     case ["7", 1, 0]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))
+                        self.graph[(w, h)].append((w + shift_w, h + shift_h))
 
                         # new coordinates
                         w += shift_w
                         h += shift_h
                         self.graph[(w, h)] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
                         shift_h = 1
                         shift_w = 0
                     case ["7", 0, -1]:
                         # add coordinate to the graph because match
-                        self.graph[(w, h)].append((w+shift_w, h+shift_h))
+                        self.graph[(w, h)].append((w + shift_w, h + shift_h))
 
                         # new coordinates
                         w += shift_w
                         h += shift_h
                         self.graph[(w, h)] = []
-                        
+
                         # next step to analyze based on shape and direction of travel
                         shift_h = 0
                         shift_w = -1
                     case _:
                         break
                 try:
-                    char = self.input[h+shift_h][w+shift_w] # 1: L [4][0]; 2: J [4][1]; 3: F [3][1]; 4: [3][2]
+                    char = self.input[h + shift_h][
+                        w + shift_w
+                    ]  # 1: L [4][0]; 2: J [4][1]; 3: F [3][1]; 4: [3][2]
                     counter += 1
                     # self.debug(char)
                 except IndexError:
                     break
             else:
-                self.graph[(w, h)].append((w+shift_w, h+shift_h))
+                self.graph[(w, h)].append((w + shift_w, h + shift_h))
             result.append(counter)
         self.debug(result)
         return (min([r for r in result if r != 0]) + 1) // 2
